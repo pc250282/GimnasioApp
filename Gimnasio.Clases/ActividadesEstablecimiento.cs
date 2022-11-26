368ffdf2d178e9ba4e3a33023b8171f6d2cf23cf
pc250282 <pablo.calvo@ncr.com>
2022-11-26 19:24:51 -0300

    a

---
+++ Gimnasio.Clases/ActividadesEstablecimiento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimnasio.Clases
+{
+    public class ActividadesEstablecimiento
+    {
+        public int idActividadEstablecimiento { get; set; }
+        public Actividad fk_idActividad { get; set; }
+        public Abono fk_idAbono { get; set; }
+        public Profesor fk_idProfesor { get; set; }
+
+    }
+}
